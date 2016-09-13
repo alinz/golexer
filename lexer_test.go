81f86cbbf8d0a833ed43accdf9a80702d704b7c7
Ali Najafizadeh <a.najafizadeh@gmail.com>
2016-09-13 17:33:41 -0400

    cleaned up and added more tests

--- lexer_test.go
+++ lexer_test.go
@@ -1,35 +1,29 @@
 package golexer
 
 import (
-	"fmt"
-	"os"
+	"bytes"
 	"testing"
 )
 
-func TestLexer(t *testing.T) {
-	file, err := os.Open("./sample/test.txt")
-	if err != nil {
-		t.Error(err)
+func TestLexerNext(t *testing.T) {
+	input := bytes.NewBufferString(`Hello World`)
+
+	matched := []rune{
+		'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd',
 	}
 
-	defer file.Close()
+	lexer := New(input, 10)
 
-	lexer := New(file, 1024)
-
-	for i := 0; i < 100; i++ {
-		lexer.AcceptRunUntil("\n")
-		fmt.Printf("->%s<- %d\n", lexer.CurrentString(), len(lexer.CurrentString()))
-		lexer.Ignore()
-
-		lexer.Next()
-		lexer.Ignore()
+	var val rune
+	for i := 0; i < len(matched); i++ {
+		val = lexer.Next()
+		if matched[i] != val {
+			t.Errorf("got '%s' instead of '%s", string(matched[i]), string(val))
+		}
 	}
 
-	// lexer.AcceptRunUntil(" ")
-	// fmt.Println(lexer.CurrentString())
-	// lexer.Ignore()
-
-	// lexer.AcceptRunUntil(" ")
-	// fmt.Println(lexer.CurrentString())
-	// lexer.Ignore()
+	val = lexer.Next()
+	if val != 0 {
+		t.Errorf("it should be 0 but got '%s'", string(val))
+	}
 }
